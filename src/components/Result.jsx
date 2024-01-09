1784009002653ee43f3523e2bd7bf26f90cd72b6
Grindmeow <thehumaneatermeow@gmail.com>
2024-01-09 18:26:15 +0545

    ch

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
-
-const Result = ({ srcCode }) => {
-  console.log('result updated');
-
-
-
+function Result({ srcCode }) {
   return (
-    <div className='bg-[#282c34] p-4 shadow mt-4 rounded-lg '>
-      <h2> Result</h2>
-      <iframe
-        className='w-full border border-gray-700 h-60 rounded-md'
-        srcDoc={srcCode}
-        title='output'
-
-        width="100%"
-        height="100%"></iframe>
-
+    <div>
+      <div className="bg-[#282c34] p-4 shadow mt-4 rounded-lg">
+        <h2
+          className="text-lg font-semibold mb-2 text-white">
+          Result
+        </h2>
+        <iframe
+          className="w-full h-60 border border-gray-700 rounded-md"
+          srcDoc={srcCode}
+          title="output"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+        >
+        </iframe>
+      </div>
     </div>
   )
 }
