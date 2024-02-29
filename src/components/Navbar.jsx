6240a6b23166fb38d939bd92ca1e8748760afece
Grindmeow <thehumaneatermeow@gmail.com>
2024-02-29 21:30:01 +0545

    pus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div
       className=' lg:flex md:flex flex-wrap justify-between  items-center px-4 bg-[#2f3640] py-4 '>
 
-      <img className=' font-bold  text-2xl text-white text-center w-44 ' src="public/images/CodePen_logo.png" alt="" />
+      <img className=' font-bold  text-2xl text-white text-center w-44 ' src="/public/images/CodePen_logo.png" alt="" />
 
     </div>
 
